43af1ec9ad275c23054fb5850fdda89ab1c6df76
Hernando Abella <hernandoabella@gmail.com>
2025-09-07 17:11:11 -0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,10 @@
 // components/DashboardLayout.tsx
+'use client';
+
 import { ReactNode } from "react";
 import Sidebar from "./../components/SideBar";
 import Chatbox from "./../components/ChatBox";
+import { SessionProvider } from "next-auth/react";
 
 interface Props {
   children: ReactNode;
@@ -9,14 +12,16 @@ interface Props {
 
 export default function DashboardLayout({ children }: Props) {
   return (
-    <div className="min-h-screen bg-gray-50 flex">
-      <Sidebar />
-      <main className="flex-1 ml-64 p-8">
-        <div className="max-w-4xl mx-auto">
-          {children}
-        </div>
-      </main>
-      <Chatbox />
-    </div>
+    <SessionProvider>
+      <div className="min-h-screen bg-gray-50 flex">
+        <Sidebar />
+        <main className="flex-1 ml-64 p-8">
+          <div className="max-w-4xl mx-auto">
+            {children}
+          </div>
+        </main>
+        <Chatbox />
+      </div>
+    </SessionProvider>
   );
 }
