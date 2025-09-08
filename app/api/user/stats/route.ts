26b87bab432010804c8cca50878758aec981f0b6
Hernando Abella <hernandoabella@gmail.com>
2025-09-08 13:32:49 -0500

    Update route.ts

--- app/api/user/stats/route.ts
+++ app/api/user/stats/route.ts
@@ -1,6 +1,6 @@
 // app/api/user/stats/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "../../../lib/prisma";
+import { prisma } from "../../../lib/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../lib/auth";
 
