99b41b7f2881cf1dcbf2889c1f187d0e02357ae5
Hernando Abella <hernandoabella@gmail.com>
2025-12-01 13:17:01 -0500

    Create server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("✅ API is running");
+});
+
+// Server
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+
